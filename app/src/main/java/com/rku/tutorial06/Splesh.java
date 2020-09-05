486dbf718da7c07c684f88b4c2a46f82b3abebdf
jbaraiya <jbaraiya254@rku.ac.in>
2020-09-05 19:42:23 +0530

    activity_splesh design added

---
+++ app/src/main/java/com/rku/tutorial06/Splesh.java
@@ -0,0 +1,14 @@
+package com.rku.tutorial06;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Splesh extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splesh);
+    }
+}
