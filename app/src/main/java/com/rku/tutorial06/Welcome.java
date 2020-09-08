ef1d8332e6e1bcba29629945e6bc3b2047fa8808
jbaraiya <jbaraiya254@rku.ac.in>
2020-09-08 16:22:46 +0530

    Welcome.java logout functionality added

--- app/src/main/java/com/rku/tutorial06/Welcome.java
+++ app/src/main/java/com/rku/tutorial06/Welcome.java
@@ -1,12 +1,20 @@
 package com.rku.tutorial06;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 public class Welcome extends AppCompatActivity {
     TextView txtWelcome;
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +22,30 @@ public class Welcome extends AppCompatActivity {
         setContentView(R.layout.activity_welcome);
         txtWelcome=findViewById(R.id.txtWelcome);
         txtWelcome.setText("Welcome: "+getIntent().getStringExtra("userdata"));
+        sharedPreferences=getSharedPreferences("student",MODE_PRIVATE);
+        editor=sharedPreferences.edit();
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater=getMenuInflater();
+        menuInflater.inflate(R.menu.custom_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId())
+        {
+            case R.id.logout:
+                editor.remove("username");
+                editor.commit();
+                Intent intent=new Intent(Welcome.this,MainActivity.class);
+                startActivity(intent);
+                break;
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
